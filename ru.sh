515e02f4ac7d0071fc34e85321e213f98f990e9d
Matteus Legat <matteuslegat@gmail.com>
2016-04-02 15:27:59 -0300

    melhorias e correção para sábado/domingo

--- ru.sh
+++ ru.sh
@@ -3,12 +3,10 @@
 # script que mostra o cardápio do RU UFSC de hoje
 #
 
-wget http://ru.ufsc.br/ru/ -q -O /tmp/ru.gz
-gunzip -d /tmp/ru.gz &> /dev/null
-mv -f /tmp/ru.gz /tmp/ru.html &> /dev/null
-mv -f /tmp/ru /tmp/ru.html &> /dev/null
+declare html=$(curl -s --compressed http://ru.ufsc.br/ru/)
 
-declare weekday=`date +%u`
+# mostra somente dia atual se não for passado o parâmetro `all`
+[ "$1" != "all" ] && declare weekday=`date +%u`
 
 case "$weekday" in
 	1) weekday="Segunda-feira";;
@@ -16,13 +14,13 @@ case "$weekday" in
 	3) weekday="Quarta-feira";;
 	4) weekday="Quinta-feira";;
 	5) weekday="Sexta-feira";;
-	6) weekday="Sabado";;
+	6) weekday="Sábado";;
 	7) weekday="Domingo";;
 esac
 
-declare beginsearch="<td><strong><span style=\"color: #333399\">$weekday"
+declare beginsearch="<td>[<strong> *]*<span style=\"color: #333399\">$weekday"
 
-cat /tmp/ru.html | sed -n "/$beginsearch/,/<\/tr>/p" | sed -e 's/<[^>]*>//g' | sed '/^ *[&nbsp;]*$/d'
+                     # busca cardapio do(s) dia(s)    # remove tags     # remove linhas e inicios em branco
+echo -e "$html" | sed -n "/$beginsearch/,/<\/tr>/p" | sed -e 's/<[^>]*>//g;/^ *[&nbsp;]*$/d;s/^[ * *\t]*//'
 
 
-rm /tmp/ru /tmp/ru.gz /tmp/ru.html &> /dev/null
